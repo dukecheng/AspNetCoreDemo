01720a01c1251e41276902d5177cee8ab37bb413
duke <dk@test.com>
2020-04-03 15:46:06 +0800

    初始化项目

---
+++ web/AspNetCoreDemo.DemoWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreDemo.DemoWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
